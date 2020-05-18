b4b9d5aa756ddc9d3ef7de91db24563e85d083d0
bit <bit@DESKTOP-2KUPKOU>
2020-05-18 19:32:12 +0900

    20200518

---
+++ src/chapter03/Song.java
@@ -0,0 +1,11 @@
+package chapter03;
+
+public class Song {
+	private String title;
+	private String artist;
+	private String album;
+	private String composer;
+	private int year;
+	private int track;
+
+}
